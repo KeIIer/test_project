f807b236ab99cfa8398d250a31648755bbe5f8d6
Keller <aaypublic@gmail.com>
2022-02-03 19:43:32 +0300

    Исправил баг с форматом времени в окнах редактирование и добавления для моделей User и Person.
    Добавил в админке отображение таблицы Permission.
    
    Починил ошибку content_type_id IS NULL, которая возникала при попытке добавить новую запись в таблицу Permission.

--- m3_project/app/actions.py
+++ m3_project/app/actions.py
@@ -1,13 +1,6 @@
-from django.contrib.auth.models import User
-from objectpack.ui import ModelEditWindow, BaseListWindow, BaseEditWindow, BaseSelectWindow, BaseWindow
-from objectpack.ui import ObjectTab
+from objectpack.ui import ModelEditWindow, TabbedEditWindow, TabbedWindow
 from objectpack.actions import ObjectPack
-from objectpack.ui import BaseEditWindow, make_combo_box
-from m3_ext.ui import all_components as ext
 from django.contrib.auth.models import User, Group, Permission, ContentType
-from m3.actions import Action
-from m3.actions import ActionPack
-from m3.actions.results import OperationResult
 from . import models
 from . import ui
 
@@ -21,7 +14,8 @@ class PersonObjectPack(ObjectPack):
     add_to_desktop = True
     add_to_menu = True
 
-    edit_window = add_window = ModelEditWindow.fabricate(model)
+    # edit_window = add_window = ModelEditWindow.fabricate(model)
+    edit_window = add_window = ui.PersonAddWindow
 
     columns = [
         {
@@ -60,29 +54,7 @@ class UserPack(ObjectPack):
 
     model = User
     add_window = edit_window = ui.UserEditWindow
-
-    add_to_desktop = True
-    add_to_menu = True
-
-
-    columns = [
-        {
-            'data_index': 'username',
-            'header': u'username',
-            'width': 2,
-        },
-        {
-            'data_index': 'email',
-            'header': u'email',
-            'width': 2,
-        }
-    ]
-
-
-class FabricateUserPack(ObjectPack):
-
-    model = User
-    add_window = edit_window = ModelEditWindow.fabricate(model)
+    # add_window = edit_window = ModelEditWindow.fabricate(model)
 
     add_to_desktop = True
     add_to_menu = True
@@ -106,6 +78,7 @@ class PermissionPack(ObjectPack):
 
     model = Permission
     add_window = edit_window = ui.PermissionEditWindow
+    # add_window = edit_window = ModelEditWindow.fabricate(model=model)
 
     add_to_desktop = True
     add_to_menu = True
